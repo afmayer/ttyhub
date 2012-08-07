8136fb7cb51db6364c44ba2ff67ae73522e263c1
Alexander F. Mayer <tinybluebutton@gmail.com>
2012-08-07 23:09:37 +0200

    Renamed SERHUB to TTYHUB
    * serhub.c -> ttyhub.c
    * all symbol names with 'serhub_' prefix now have a 'ttyhub_' prefix
    * also uppercase symbols (i.e. N_SERHUB -> N_TTYHUB)
    * changed filename in Makefile
    * renamed directory
    
    Signed-off-by: Alexander F. Mayer <tinybluebutton@gmail.com>

---
+++ ttyhub/ttyhub.c
@@ -0,0 +1,102 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/tty.h>
+#include <linux/slab.h>
+MODULE_LICENSE("GPL");
+
+#define N_TTYHUB 29
+#if N_TTYHUB >= NR_LDISCS
+#error N_TTYHUB is larger than the maximum allowed value
+#endif
+
+struct ttyhub
+{
+        char txbuf[32];
+        char rxbuf[32]; // TODO alloc buffers, size configurable
+};
+
+static int ttyhub_open(struct tty_struct *tty)
+{
+        struct ttyhub *data;
+        int err = -ENOBUFS;
+
+        data = kmalloc(sizeof(*data), GFP_KERNEL);
+        if (data == NULL)
+                goto error_exit;
+
+        /* success */
+        return 0;
+
+error_exit:
+        return err;
+}
+
+static void ttyhub_close(struct tty_struct *tty)
+{
+}
+
+static int ttyhub_ioctl(struct tty_struct *tty, struct file *filp,
+                        unsigned int cmd, unsigned long arg)
+{
+        return -ENOTTY;
+}
+
+static int ttyhub_hangup(struct tty_struct *tty)
+{
+        ttyhub_close(tty);
+        return 0;
+}
+
+static void ttyhub_receive_buf(struct tty_struct *tty, const unsigned char *cp,
+                        char *fp, int count)
+{
+        while (count--)
+        {
+                // TODO receive char by char
+        }
+}
+
+static void ttyhub_write_wakeup(struct tty_struct *tty)
+{
+        int written;
+
+        //written = tty->ops->write(tty, pointer_to_first_buffer_byte, bytes_in_buffer);
+        // TODO update buffer pointer, bytes_in_buffer
+}
+
+struct tty_ldisc_ops ttyhub_ldisc =
+{
+        .owner        = THIS_MODULE,
+        .magic        = TTY_LDISC_MAGIC,
+        .name         = "ttyhub",
+        .open         = ttyhub_open,
+        .close        = ttyhub_close,
+        .ioctl        = ttyhub_ioctl,
+        .hangup       = ttyhub_hangup,
+        .receive_buf  = ttyhub_receive_buf,
+        .write_wakeup = ttyhub_write_wakeup
+};
+
+static int ttyhub_init(void)
+{
+        int status;
+
+        printk(KERN_INFO "TTYHUB initialization start\n");
+        status = tty_register_ldisc(N_TTYHUB, &ttyhub_ldisc);
+        return 0;
+}
+
+static void ttyhub_exit(void)
+{
+        int status;
+
+        printk(KERN_INFO "TTYHUB exiting\n");
+        status = tty_unregister_ldisc(N_TTYHUB);
+        if (status != 0)
+                printk(KERN_ERR "TTYHUB: can't unregister line "
+                        "discipline (err = %d)\n", status);
+}
+
+module_init(ttyhub_init);
+module_exit(ttyhub_exit);
+
