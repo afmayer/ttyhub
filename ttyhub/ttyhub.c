6f188adf074906f166b93fbb9f5ec1ec4b2de40a
Alexander F. Mayer <tinybluebutton@gmail.com>
2012-08-08 07:05:43 +0200

    * renamed ttyhub structure to ttyhub_state      - now stored in tty_struct (disc_data field)      - proper initialization in ttyhub_open() * renamed "maxsubsys" module parameter to "max_subsys" * new module parameter "probe_buf_size", default 32 bytes, minimum 16 * no more fixed sized buffers in ttyhub_state struct * probe buffer, part of ttyhub_state, allocation at ttyhub_open(), memory     released at ttyhub_close()
    Signed-off-by: Alexander F. Mayer <tinybluebutton@gmail.com>

--- ttyhub/ttyhub.c
+++ ttyhub/ttyhub.c
@@ -12,34 +12,53 @@ MODULE_LICENSE("GPL");
 #error N_TTYHUB is larger than the maximum allowed value
 #endif
 
-static int maxsubsys = 16;
-module_param(maxsubsys, int, 0);
-MODULE_PARM_DESC(maxsubsys, "Maximum number of TTYHUB subsystems");
+static int max_subsys = 16;
+module_param(max_subsys, int, 0);
+MODULE_PARM_DESC(max_subsys, "Maximum number of TTYHUB subsystems");
 
-struct ttyhub
+static int probe_buf_size = 32;
+module_param(probe_buf_size, int, 0);
+MODULE_PARM_DESC(probe_buf_size, "Size of the TTYHUB receive probe buffer");
+
+struct ttyhub_state
 {
-        char txbuf[32];
-        char rxbuf[32]; // TODO alloc buffers, size configurable
+        int recv_subsys;
+        unsigned char *probe_buf;
 };
 
 static int ttyhub_open(struct tty_struct *tty)
 {
-        struct ttyhub *data;
+        struct ttyhub_state *state;
         int err = -ENOBUFS;
 
-        data = kmalloc(sizeof(*data), GFP_KERNEL);
-        if (data == NULL)
+        state = kmalloc(sizeof(*state), GFP_KERNEL);
+        if (state == NULL)
                 goto error_exit;
+        state->recv_subsys = -1;
+        state->probe_buf = kmalloc(probe_buf_size, GFP_KERNEL);
+        if (state->probe_buf == NULL)
+                goto error_cleanup_state;
 
         /* success */
+        tty->disc_data = state;
         return 0;
 
+error_cleanup_state:
+        kfree(state);
 error_exit:
         return err;
 }
 
 static void ttyhub_close(struct tty_struct *tty)
 {
+        struct ttyhub_state *state = tty->disc_data;
+
+        if (state == NULL)
+                return;
+
+        if (state->probe_buf != NULL)
+                kfree(state->probe_buf);
+        kfree(state);
 }
 
 static int ttyhub_ioctl(struct tty_struct *tty, struct file *filp,
@@ -57,6 +76,9 @@ static int ttyhub_hangup(struct tty_struct *tty)
 static void ttyhub_receive_buf(struct tty_struct *tty, const unsigned char *cp,
                         char *fp, int count)
 {
+        // TODO conditional output if debug var set
+        printk(KERN_INFO "ttyhub_receive_buf() called with count=%d\n", count);
+
         while (count--)
         {
                 // TODO receive char by char
@@ -88,11 +110,14 @@ static int ttyhub_init(void)
 {
         int status;
 
-        if (maxsubsys < 2)
-                maxsubsys = 2;
+        if (max_subsys < 2)
+                max_subsys = 2;
 
-        printk(KERN_INFO "TTYHUB: version %s, max. subsystems = %d\n",
-                TTYHUB_VERSION, maxsubsys);
+        if (probe_buf_size < 16)
+                probe_buf_size = 16;
+
+        printk(KERN_INFO "TTYHUB: version %s, max. subsystems = %d, probe bufsize"
+                " = %d\n", TTYHUB_VERSION, max_subsys, probe_buf_size);
         status = tty_register_ldisc(N_TTYHUB, &ttyhub_ldisc);
         return 0;
 }
