ea6f4329d46a93d9a6a5ec4093d61bac032622d5
Alexander F. Mayer <tinybluebutton@gmail.com>
2012-08-08 23:53:35 +0200

    First steps for receive state machine.
    * unfinished versions of receive state machine helper functions
        ttyhub_probe_subsystems() and ttyhub_probe_subsystems_size()
    * comment how the receive state machine works
    * (part of) backbone of receive state machine
    * new field in subsystem struct: minimum bytes for probe_data()
    * more comments
    
    Signed-off-by: Alexander F. Mayer <tinybluebutton@gmail.com>

--- ttyhub/ttyhub.c
+++ ttyhub/ttyhub.c
@@ -34,13 +34,37 @@ struct ttyhub_subsystem {
         int (*probe_data)(void);
         int (*probe_size)(void);
         int (*do_receive)(void);
+
+        /* minimum bytes received before probing the submodule */
+        int probe_data_minimum_bytes;
 };
 
 
 static struct ttyhub_subsystem **ttyhub_subsystems;
 static struct semaphore ttyhub_subsystems_sem;
 
-/* line discipline operations */
+/*
+ * Probe subsystems if they can identify a received data chunk.
+ * The ttyhub_state structure is changed according to the probe results.
+ * This is a helper function for ttyhub_receive_buf().
+ *
+ * All locks to involved data structures are asssumed to be held already.
+ *
+ * Returns:
+ *  0   either a subsystem has identified the data or all subsystems have
+ *      already been probed - state machine must continue in this call
+ *  1   the received data has not been identified but may be later with
+ *      more data - probe buffer has been filled, call can return
+ */
+static int ttyhub_probe_subsystems(struct ttyhub_state *state)
+{
+        // TODO implement
+}
+
+static int ttyhub_probe_subsystems_size(struct ttyhub_state *state)
+{}
+
+/* Line discipline open() operation */
 static int ttyhub_open(struct tty_struct *tty)
 {
         struct ttyhub_state *state;
@@ -74,6 +98,7 @@ error_exit:
         return err;
 }
 
+/* Line discipline close() operation */
 static void ttyhub_close(struct tty_struct *tty)
 {
         struct ttyhub_state *state = tty->disc_data;
@@ -88,19 +113,59 @@ static void ttyhub_close(struct tty_struct *tty)
         kfree(state);
 }
 
+/* Line discipline ioctl() operation */
 static int ttyhub_ioctl(struct tty_struct *tty, struct file *filp,
                         unsigned int cmd, unsigned long arg)
 {
         return -ENOTTY;
 }
 
-static void ttyhub_receive_buf(struct tty_struct *tty, const unsigned char *cp,
-                        char *fp, int count)
+/*
+ * Line discipline ioctl() operation
+ * Called by the hardware driver when new data arrives.
+ */
+static void ttyhub_receive_buf(struct tty_struct *tty,
+                        const unsigned char *cp, char *fp, int count)
 {
         struct ttyhub_state *state = tty->disc_data;
+        int status;
 
         // TODO conditional output if debug var set
         printk(KERN_INFO "ttyhub_receive_buf() called with count=%d\n", count);
+
+        /* Receive state machine:
+         * The relevant fields in the ttyhub_state struct are:
+         *   1) recv_subsys
+         *        values from 0 to (max_subsys-1):
+         *              addressed subsystem is known, proceed with receiving
+         *              data immediately
+         *        value -1:
+         *              addressed subsystem is unknown and has to be probed
+         *        value -2:
+         *              addressed subsystem is unknown and all registered
+         *              subsystems have already been probed
+         *   2) probed_subsystems
+         *        This is a pointer to an unsigned char array containing one
+         *        bit for every possible subsystem. When the addressed
+         *        subsystem is unknown a set bit indicates that the subsystem
+         *        has already been probed and should not be probed again.
+         */
+        while (1) {
+                if (state->recv_subsys == -1) {
+                        /* this may change recv_subsys to -2 or a nonnegative
+                           value which then gets processed in the same call */
+                        status = ttyhub_probe_subsystems(state);
+                        if (status)
+                                /* not enough data to probe all subsystems */
+                                return;
+                }
+                if (state->recv_subsys == -2) {
+                        status = ttyhub_probe_subsystems_size(state);
+                }
+                if (state->recv_subsys >= 0) {
+                        ttyhub_subsystems[state->recv_subsys]->do_receive();
+                }
+        }
 }
 
 static void ttyhub_write_wakeup(struct tty_struct *tty)
