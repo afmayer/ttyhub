fb06e2cab5a45383ff1356931c37833bf1f659ed
Alexander F. Mayer <tinybluebutton@gmail.com>
2013-07-16 21:27:44 +0200

    GPLv2 (or later) header for sourcefiles and headers + MODULE_AUTHOR.

--- modules/testsubsys0/testsubsys0.c
+++ modules/testsubsys0/testsubsys0.c
@@ -1,9 +1,26 @@
-/* Copyright (C) 2012 Alexander F. Mayer */
+/* ttyhub test subsystem 0
+ * Copyright (c) 2013 Alexander F. Mayer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/tty.h>
 #include <linux/slab.h>
 #include "ttyhub.h"
+MODULE_AUTHOR("Alexander F. Mayer");
 MODULE_LICENSE("GPL");
 
 struct testsubsys0_data {
--- modules/ttyhub/ttyhub.c
+++ modules/ttyhub/ttyhub.c
@@ -1,5 +1,19 @@
-/* Copyright (C) 2012 Alexander F. Mayer */
-// TODO write a reasonable file header (copyright, license, ...)
+/* ttyhub
+ * Copyright (c) 2013 Alexander F. Mayer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -12,6 +26,7 @@
 #include <linux/jiffies.h>
 #include "ttyhub.h"
 #include "ttyhub_ioctl.h"
+MODULE_AUTHOR("Alexander F. Mayer");
 MODULE_LICENSE("GPL");
 
 #define TTYHUB_VERSION "0.20"
